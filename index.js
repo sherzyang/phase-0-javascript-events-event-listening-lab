00bee0431b0781692254595bab42cfd7f6345971
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-02-11 20:03:49 -0800

    Update.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function addingEventListener() {
-    const input = document.getElementById('button');
-
-    function clickAlert() {
-    alert('I was clicked!');
-    }
-
-    input.addEventListener('click', clickAlert);
+  const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
+
+addingEventListener()
